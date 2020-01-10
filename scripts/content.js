a23eec3982c119fc001be75cdb7abfa626f1faea
afeiship <1290657123@qq.com>
2020-01-10 17:50:52 +0800

    fix: dom not ready

--- scripts/content.js
+++ scripts/content.js
@@ -3,15 +3,15 @@ console.log('content ready!!');
 $(document).ready(function() {
   var url = location.href;
   var suffix = '&developer=true';
-  var isSsoPage = location.href.includes('sso-login.html');
   var isDev = location.href.includes(suffix);
-  var domContext = $('.ab-wp.default-show:visible');
-  var inputs = $('input', domContext);
-  var btn = $('.new-btn', domContext);
 
   if (!isDev) {
     location.href = url + suffix;
   }
+
+  var domContext = $('.ab-wp.default-show:visible');
+  var inputs = $('input', domContext);
+  var btn = $('.new-btn', domContext);
 
   if (isSsoPage) {
     inputs[0].focus();
