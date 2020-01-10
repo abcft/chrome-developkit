534fbf0892be3a7357e57aee0034ce5a4cd4c742
afeiship <1290657123@qq.com>
2020-01-10 17:46:00 +0800

    add: user/pass

--- scripts/content.js
+++ scripts/content.js
@@ -5,11 +5,20 @@ $(document).ready(function() {
   var suffix = '&developer=true';
   var isSsoPage = location.href.includes('sso-login.html');
   var isDev = location.href.includes(suffix);
-  // var domContext = $('.ab-wp.default-show:visible');
-  // var inputs = $('input', domContext);
-  // var btn = $('.new-btn', domContext);
+  var domContext = $('.ab-wp.default-show:visible');
+  var inputs = $('input', domContext);
+  var btn = $('.new-btn', domContext);
 
-  if (isSsoPage && !isDev) {
+  if (!isDev) {
     location.href = url + suffix;
+  }
+
+  if (isSsoPage) {
+    inputs[0].focus();
+    inputs[0].value = 'test5@abcft.com';
+    inputs[1].focus();
+    inputs[1].value = '1234qwer';
+    btn.text('🐶已经填好啦!');
+    // btn.click();
   }
 });
